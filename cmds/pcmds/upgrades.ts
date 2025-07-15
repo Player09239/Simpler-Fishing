d9788f3ca0e17268cc991bb3aea327544c0d927a
Chaos <destroyer09239@gmail.com>
2025-07-15 10:14:34 -0400

    Create upgrades.ts

---
+++ cmds/pcmds/upgrades.ts
@@ -0,0 +1,53 @@
+import { EmbedBuilder } from 'discord.js'
+import bot from './../../data/bot.ts'
+import user from './../../data/user.ts'
+import { cmdup, msgup } from './../../util/func.ts'
+
+export async function upgrades(message: any): Promise<void> {
+	try {
+		let b: any = await bot.findOne({ botId: '1389387486035443714' })
+		if (message.content.startsWith(`${b.prefix}upgrades`)) {
+			let u: any = await user.findOne({ userId: message.author.id })
+			msgup()
+			cmdup()
+
+			const args = message.content.split('').filter(Boolean)
+			const upgrade = args[1]
+			switch (upgrade) {
+				case 'fishing':
+					if (u.cash >= 300) {
+						u.cash -= 300
+						u.upgrades.fishing += 1
+						message.reply('Successful')
+						break
+					} else {
+						message.reply('Failure')
+						break
+					}
+				case 'cash':
+					if (u.cash >= 350) {
+						u.cash -= 350
+						u.upgrades.cash += 1
+						message.reply('Successful')
+						break
+					} else {
+						message.reply('Failure')
+						break
+					}
+				default:
+					const upgrades = new EmbedBuilder()
+        				.setDescription(`
+## UPGRADES
+-# ${u.name}
+
+Fishing I
+Cash I
+                        `)
+    				message.reply({ embeds: [upgrades] })
+					break
+			}
+		}
+	} catch (error) {
+		throw new Error(`upgrades.ts > Error: ${error}`)
+	}
+}
